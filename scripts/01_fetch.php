982064d41efe4746a33ccb649d84169a8ab3995e
Finjon Kiang <kiang@w510sd.olc.tw>
2020-10-09 00:36:43 +0800

    force update page file

--- scripts/01_fetch.php
+++ scripts/01_fetch.php
@@ -1,9 +1,7 @@
 <?php
 include __DIR__ . '/CJSON.php';
 $pageFile = __DIR__ . '/page';
-if(!file_exists($pageFile)) {
-    file_put_contents($pageFile, file_get_contents('https://wdms.epa.gov.tw/idms/public/ctrlcase.aspx'));
-}
+file_put_contents($pageFile, file_get_contents('https://wdms.epa.gov.tw/idms/public/ctrlcase.aspx'));
 $json = new CJSON();
 $page = file_get_contents($pageFile);
 $pos = strpos($page, '[{\'CSID\'');
